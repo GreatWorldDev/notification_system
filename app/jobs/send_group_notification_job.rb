f24494485fd169d01f1c142c616f8e917d89e614
greatworlddev <happyfly.dev@gmail.com>
2025-03-25 01:38:36 -0600

    fix group find nil check and batch processing in group notification

--- app/jobs/send_group_notification_job.rb
+++ app/jobs/send_group_notification_job.rb
@@ -4,10 +4,14 @@ class SendGroupNotificationJob
   sidekiq_options retry: 3
 
   def perform(group_id, notification_params)
-    group = Group.find_by(id: group_id)
+    group = Group.find(group_id) # find will raise RecordNotFound if not found, handled in application_contrller.rb
 
+    # Optimized: Batch user IDs into groups of 100 and use perform_bulk to enqueue jobs,
+    # reducing the overhead compared to enqueuing each job individually.
     group.users.pluck(:id).each_slice(100) do |user_ids|
-      user_ids.each { |user_id| SendNotificationJob.perform_async(user_id, notification_params) }
+      SendNotificationJob.perform_bulk(
+        user_ids.map { |user_id| [ user_id, notification_params ] }
+      )
     end
 
     Rails.logger.info("Queued notifications for all users in the group #{group_id}")
